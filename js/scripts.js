a6ce37478aefb6877ffab137d8179c93fc64cb0d
Gelber Arruda <junior@informaticabetel.com.br>
2020-05-01 17:52:35 -0300

    First CPF validation

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,10 @@ console.log("JavaScript carregado")
 function valida() {
     const cpf = document.getElementById("cpf").value
     console.log(`CPF: ${cpf}`)
+    if(cpf.length==11) { 
+        document.getElementById('success').style.display = 'block';
+    }
+
 }
 
 
