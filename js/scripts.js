065f576bd9689148b6a6b2ff55cddb13c68f5bed
Gelber Arruda <junior@informaticabetel.com.br>
2020-05-01 18:12:11 -0300

    Minor fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,28 @@
-console.log("JavaScript carregado")
 
 function valida() {
+    const sucessAlert = getById('success')
+    const failedAlert = getById('failed')
+    setVisibility(sucessAlert, 0)
+    setVisibility(failedAlert, 0)
+
     const cpf = document.getElementById("cpf").value
-    console.log(`CPF: ${cpf}`)
-    if(cpf.length==11) { 
-        document.getElementById('success').style.display = 'block';
+    if (cpf.length == 11) {
+        setVisibility(sucessAlert, 1)
+    } else {
+        setVisibility(failedAlert, 1)
     }
 
 }
 
+function getById(id) {
+    return document.getElementById(id);
+}
 
+function setVisibility(element, status) {
+    if (status) {
+        element.style.display = 'block'
+    } else {
+        element.style.display = 'none'
+    }
+
+}
